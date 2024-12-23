14515160c709cf7360b4e3e509cacb25186e4342
DESKTOP-DOMPF27\yazan <t.yazan@int.fs.de>
2024-12-23 16:26:35 +0100

    Initial commit: Spring Boot Life Sync Project

---
+++ src/main/java/com/example/lifesync/LifeSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifesync/LifeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
